c3db33ec6c2689d49cdac31bb9d34fb2570749be
vodkavelvet <158987465+vodkavelvet@users.noreply.github.com>
2024-12-23 12:19:05 +0700

    W

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Transform player;
+    public GameObject colorCircle, powerUp;
+    public Vector3 spawnPosition;
+    public int minSpawnDistance, maxSpawnDistance;
+    public int minRotationSpeed, maxRotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y > transform.position.y)
+            transform.position = new Vector3(0, player.position.y, transform.position.z);
+        if(player.transform.position.y + transform.position.y < -7)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if(Mathf.Abs(transform.position.y - spawnPosition.y ) < 6)
+        {
+            if (Random.Range(0, 5) == 0)
+            {
+                spawnPowerUp();
+            }
+            else
+            {
+                spawnCircle(Random.Range(minRotationSpeed, maxRotationSpeed) * 10);
+            }
+        }
+    }
+
+    void spawnCircle(int speed)
+    {
+        GameObject tempCircle = Instantiate(colorCircle , spawnPosition, Quaternion.identity);
+        tempCircle.GetComponent<CircleBehaviour>().rotationSpeed = speed;
+        spawnPosition += new Vector3(0,Random.Range(minSpawnDistance,maxSpawnDistance), 0);
+    }
+    void spawnPowerUp()
+    {
+        GameObject tempCircle = Instantiate(powerUp, spawnPosition, Quaternion.identity);
+        spawnPosition += new Vector3(0, Random.Range(minSpawnDistance, maxSpawnDistance), 0);
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public int jumpingForce;
+    public Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
+    public GameObject destroyedPrefab;
+    public string currentColor;
+
+    void Start()
+    {
+        int index = Random.Range(0, 4);
+        currentColor = setColor(index);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpingForce;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "PowerUp")
+        {
+            currentColor = setColor(other.GetComponent<PowerUpBehaviour>().colorIndex);
+            Destroy(other.gameObject);
+        }
+        else
+        {
+            if (other.gameObject.tag != currentColor)
+            {
+                StartCoroutine(cameraJig());
+                spriteRenderer.enabled = false;
+            }
+        }
+    }
+
+    IEnumerator cameraJig()
+    {
+        Camera.main.GetComponent<Animation>().Play();
+        yield return new WaitForSeconds(.8f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    string setColor(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                spriteRenderer.color = Color.cyan;
+                return "blue";
+            case 1:
+                spriteRenderer.color = Color.yellow;
+                return "yellow";
+            case 2:
+                spriteRenderer.color = Color.magenta;
+                return "purple";
+            case 3:
+                spriteRenderer.color = new Color(255,0,125);
+                return "pink";
+        }
+        return "blue";
+    }
+}
